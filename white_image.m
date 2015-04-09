5d1cc59ed9cd437c5b5288569d695f89968bb735
Alex Lanz <alex@krumer.it>
2015-04-09 13:08:58 +0200

    Creation of white image

---
+++ white_image.m
@@ -0,0 +1,23 @@
+% Watermark Detector
+
+clear all;
+
+image_name='images/baboon.bmp';
+output_name='outputs/white.bmp';
+
+% read in the images
+image_object=imread(image_name);
+
+% determine size of watermark image
+Mw=size(image_object,1);    %Height
+Nw=size(image_object,2);    %Width
+
+% use msb of watermark image
+for ii = 1:Mw
+    for jj = 1:Nw
+        image_object(ii,jj)=0;
+    end
+end
+
+% output
+imwrite(image_object, output_name);
