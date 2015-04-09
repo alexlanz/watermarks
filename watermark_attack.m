67f76fbc2a6ff884118f01fa0f0ed8a7d42f6f26
Alex Lanz <alex@krumer.it>
2015-04-09 13:39:33 +0200

    Adding first step of attack

--- watermark_attack.m
+++ watermark_attack.m
@@ -1,31 +1,49 @@
-% Watermark Detector
+% Watermark Attack
 
 clear all;
 
-watermark_name='images/baboon.bmp';
 
-% read in the images
-watermark_object=imread(watermark_name);
+image_name='outputs/watermarked.bmp';
+image_object=imread(image_name);
 
-% determine size of watermark image
-Mw=size(watermark_object,1);    %Height
-Nw=size(watermark_object,2);    %Width
 
-% use msb of watermark image
-for ii = 1:Mw
-    for jj = 1:Nw
-        watermark_original(ii,jj)=bitget(watermark_object(ii,jj),8);
+function return_value = watermark_detector( image_object ) 
+ 
+    return_value = 0;
+
+    watermark_name='images/baboon.bmp';
+    watermark_threshold=0.8;
+
+    watermark_object=imread(watermark_name);
+
+    Sh=size(watermark_object,1);
+    Sw=size(watermark_object,2);
+
+    % use msb of watermark image
+    for ii = 1:Sh
+        for jj = 1:Sw
+            watermark_original(ii,jj)=bitget(watermark_object(ii,jj),8);
+        end
     end
+
+    % use lsb of watermarked image
+    for ii = 1:Sh
+        for jj = 1:Sw
+            watermark_extracted(ii,jj)=bitget(image_object(ii,jj),1);
+        end
+    end
+
+    % check the difference of the original watermark and the extracted one
+    d=corr2(watermark_original,watermark_extracted);
+
+    if (d > watermark_threshold)
+        return_value = 1;
+    end
+ 
 end
 
 
-% create start white image
-image_object=true(Mw,Nw);
 
-% output
-imwrite(image_object, 'outputs/white.bmp');
-
-
-image_object=imread('outputs/white.bmp');
-image_object(1:256,1:256) = [1,0,0];
-imwrite(image_object, 'outputs/white.bmp');
+% Initial check
+result = watermark_detector(image_object);
+display(result);
