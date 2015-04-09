08b307a7cc7cde1486b20b2bba2245ca4d6cf253
Alex Lanz <alex@krumer.it>
2015-04-09 09:20:41 +0200

    Adding diff to compare original and watermarked image

---
+++ watermark_diff.m
@@ -0,0 +1,16 @@
+% Watermark Difference
+
+clear all;
+
+image_name='images/lena.bmp';
+watermarked_name='outputs/watermarked.bmp';
+output_name='output/diff.bmp';
+
+% read in the images
+image_object=imread(image_name);
+watermarked_object=imread(watermarked_name);
+
+% create difference of original and watermarked image
+diff_object=imfuse(image_object,watermarked_object,'diff');
+
+imwrite(diff_object, output_name);
