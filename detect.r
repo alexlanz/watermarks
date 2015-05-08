2aa37d338b90d57fd6f9069a4131e0fe55e46231
Alex Lanz <alex@krumer.it>
2015-05-08 10:15:06 +0200

    First version of correlation

--- detect.r
+++ detect.r
@@ -5,18 +5,13 @@ library(png)
 # Load the watermark
 watermark <- as.matrix(read.table("outputs/watermark", header=F))
 
-# Load watermarked image
-#watermarked_png <- load.png("outputs/watermarked.png")
-img <- readPNG("outputs/watermarked.png")
-print(img)
-
-watermarked <- t(as.matrix(watermarked_png))
-print(watermarked)
-
+# Load the image
+image <- load.png("outputs/watermarked.png")
+image <- as.matrix(image) * 255
 
 # Pearson correlation
 print(dim(watermark))
-print(dim(watermarked))
-result <- cor.test(watermarked, watermark, method="pearson")
+print(dim(image))
+result <- cor.test(image, watermark, method="pearson")
 
 print(result)
